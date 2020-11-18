18e1df1fd641625a03a7034353705c4da86b8a64
Julien MARTIN-PRIN <julienmartinprin@gmail.com>
2020-11-18 14:29:49 +0100

    Added the player class

---
+++ code/objects/player.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+# Authors: Julien MARTIN-PRIN
+# IOS 2 Promo 2022
+
+"""
+
+Class Player
+
+"""
+
+class Player:
+	def __init__(self, _id, name):
+		"""
+		Initialize the player
+		:param self: the player itself
+		:param _id: player id
+		:param name: the player name
+		"""
+		self._id = _id
+		self.name = name
+		self.rank = 0
+		self.score = 0
+		self.role = ""
+
+	def __str__(self):
+		"""
+		Description of the player
+		:param self: the player itself
+		:return: player's description
+		"""
+		return ('Player name: ' + str(name)
+			+ 'Current rank: ' + str(rank))
