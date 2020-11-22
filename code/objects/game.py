7fd32b263cafb58cb6bd18898ff6bb3694bf9f7a
Freshm4at <fernandesvalentin1235@gmail.com>
2020-11-22 17:08:44 +0100

    nothing

---
+++ code/objects/game.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+# Authors: Julien MARTIN-PRIN
+# IOS 2 Promo 2022
+
+"""
+
+Class game
+
+"""
+
+class Game:
+
+    def __init__(self,):
+        self.List_player
+
+    def __str__(self):
+
