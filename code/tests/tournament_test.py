047714a1ac439b6a18abd81c6b4af954005033c8
Julien MARTIN-PRIN <julienmartinprin@gmail.com>
2020-11-29 11:08:34 +0100

    Changed the name of the directory unittest to tests

---
+++ code/tests/tournament_test.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+# Authors: Julien MARTIN-PRIN & Valentin FERNANDES
+
+import unittest
+from tournament import *
+
+class TournamentTest(unittest.TestCase):
+
+
+if __name__ == '__main__':
+	main()
